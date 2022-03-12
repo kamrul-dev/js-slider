1cbbf509f7f13a4347aeebf94f75cd22c32cc503
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-12 22:27:32 +0600

    Accessing array element with time interval

---
+++ js/slider.js
@@ -0,0 +1,19 @@
+// image src in array
+const images = [
+    'images/photo1.jpg',
+    'images/photo2.jpg',
+    'images/photo3.jpg',
+    'images/photo4.jpg',
+    'images/photo5.jpg'
+]
+
+// set time interval for accessing array elemetns
+let imgIndex = 0;
+setInterval( () =>{
+    if(imgIndex >= images.length){
+        imgIndex = 0;
+    }
+    const imgUrl = images[imgIndex];
+    console.log(imgUrl);
+    imgIndex++;
+}, 1000)
