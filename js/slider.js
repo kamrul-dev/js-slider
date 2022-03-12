d8d503d0bf834caa21d595adcdd74f7eb73d1772
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-12 22:33:38 +0600

    Change src attribute url by  geting dom operation

--- js/slider.js
+++ js/slider.js
@@ -9,11 +9,13 @@ const images = [
 
 // set time interval for accessing array elemetns
 let imgIndex = 0;
+const imgElement = document.getElementById('slider-img');
 setInterval( () =>{
     if(imgIndex >= images.length){
         imgIndex = 0;
     }
     const imgUrl = images[imgIndex];
+    imgElement.setAttribute('src', imgUrl);
     console.log(imgUrl);
     imgIndex++;
 }, 1000)
